65fd856df62ef5351731aa5ca7c9b7b7a7da409f
dedbee <160147108+dedbee@users.noreply.github.com>
2025-03-03 19:29:55 +0300

    Update application.go

--- internal/application/application.go
+++ internal/application/application.go
@@ -1,1 +1,136 @@
+package application
 
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/dedbee/Calcserv_Go/internal/orchestrator"
+)
+
+type Config struct {
+	Addr string
+}
+
+func ConfigFromEnv() *Config {
+	config := new(Config)
+	config.Addr = os.Getenv("PORT")
+	if config.Addr == "" {
+		config.Addr = "8080"
+	}
+	return config
+}
+
+type Application struct {
+	config *Config
+}
+
+func New() *Application {
+	return &Application{
+		config: ConfigFromEnv(),
+	}
+}
+
+func (a *Application) RunServer() error {
+	http.HandleFunc("/api/v1/calculate", CalculateHandler)
+	http.HandleFunc("/api/v1/expressions", ExpressionsHandler)
+	http.HandleFunc("/api/v1/expressions/", ExpressionByIDHandler)
+	http.HandleFunc("/internal/task", TaskHandler)
+	return http.ListenAndServe(":"+a.config.Addr, nil)
+}
+
+type Request struct {
+	Expression string `json:"expression"`
+}
+
+func CalculateHandler(w http.ResponseWriter, r *http.Request) {
+	request := new(Request)
+	defer r.Body.Close()
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Internal server error"})
+		return
+	}
+
+	id, err := orchestrator.AddExpression(request.Expression)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Internal server error"})
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]int{"id": id})
+}
+
+func ExpressionsHandler(w http.ResponseWriter, r *http.Request) {
+	exprs, err := orchestrator.GetExpressions()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Internal server error"})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{"expressions": exprs})
+}
+
+func ExpressionByIDHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/api/v1/expressions/"):]
+	exprID, err := strconv.Atoi(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Expression not found"})
+		return
+	}
+
+	expr, err := orchestrator.GetExpressionByID(exprID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Expression not found"})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{"expression": expr})
+}
+
+func TaskHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		task, err := orchestrator.GetTask()
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"error": "No tasks available"})
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{"task": task})
+	case http.MethodPost:
+		var data map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Invalid request body"})
+			return
+		}
+
+		id := int(data["id"].(float64))
+		result := data["result"].(float64)
+
+		err = orchestrator.SubmitTaskResult(id, result)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Internal server error"})
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
